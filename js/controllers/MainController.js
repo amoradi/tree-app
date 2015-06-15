d63cfa1797509950be8436290e07719e8133e1e6
Aaron Moradi <moradi.aaron@gmail.com>
2015-06-14 21:06:31 -0400

    Added Roman Numeral fn

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -43,12 +43,185 @@ app.controller('MainController', ['$scope', function($scope) {
 				text: 'Father, Amram',
 				scripture: 'Ex.12.49'
 	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
 	        },
 	        {
+	        	submilestone: 'subname here',
 				text: 'Father, Amram',
-				scripture: 'Ex.7.20'  
+				scripture: 'Ex.12.49'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        },
+	        {
+	        	submilestone: 'subname here',
+				text: 'Father, Amram',
+				scripture: 'Ex.12.49'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
+	        }
+	      ]
+	    },
+	    {
+	      "name": 'family 2',
+	      "points": [
+	        {
+	          	text: 'Father, Amram',
+	          	scripture: 'Ex.6.90'
+	          
 	        }
 	      ]
 	    }
 	];
+
+	// @desc return roman numeral of #'s 1-39
+	$scope.convertToRomanNumeral = function(x) {
+		if (isNaN(x)) return null;
+		var y = Math.abs(Math.floor(x)),
+			y = y.toString();
+
+		function oneThruNine(i) {
+			switch (i) {
+				case '0': 
+					return '';
+					break;
+				case '1': 
+					return 'I';
+					break;
+				case '2': 
+					return 'II';
+					break;
+				case '3': 
+					return 'III';
+					break;
+				case '4': 
+					return 'IV';
+					break;
+				case '5': 
+					return 'V';
+					break;
+				case '6': 
+					return 'VI';
+					break;
+				case '7': 
+					return 'VII';
+					break;
+				case '8': 
+					return 'VIII';
+					break;
+				case '9': 
+					return 'IX';
+					break;
+			}
+		}
+
+		// single digit #
+		if (y.length === 1) {
+			return oneThruNine(y);
+		}
+		// 2 digit #
+		else {
+			if (y[0] === '1') {
+				return 'X' + oneThruNine(y[1])
+			}
+			else if (y[0] === '2') {
+				return 'XX' + oneThruNine(y[1])
+			}
+			else if (y[0] === '3') {
+				return 'XXX' + oneThruNine(y[1])
+			}
+			else {
+				return 'too large!'
+			}
+		}
+	}
+
+	//
+	point
 }]);
